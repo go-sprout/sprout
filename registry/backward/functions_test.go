dad2e734c9476553c1af3b5b3179eb596158f2d6
42Atomys <contact@atomys.fr>
2024-08-06 22:44:57 +0200

    test: backward and semver registries

--- registry/backward/functions_test.go
+++ registry/backward/functions_test.go
@@ -15,3 +15,29 @@ func TestFail(t *testing.T) {
 
 	pesticide.RunMustTestCases(t, backward.NewRegistry(), tc)
 }
+
+func TestUrlParse(t *testing.T) {
+
+	var tc = []pesticide.TestCase{
+		{Input: `{{ urlParse "https://example.com" | urlJoin }}`, Expected: "https://example.com"},
+		{Input: `{{ urlParse "https://example.com/path" | urlJoin }}`, Expected: "https://example.com/path"},
+		{Input: `{{ urlParse "https://user:pass@example.com/path?query=1" | urlJoin }}`, Expected: "https://user:pass@example.com/path?query=1"},
+	}
+
+	pesticide.RunTestCases(t, backward.NewRegistry(), tc)
+}
+
+func TestGetHostByName(t *testing.T) {
+	ipv6 := `^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$`
+	ipv4 := `^(((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4})`
+
+	ipAddressRegexp := ipv4 + `|` + ipv6
+
+	var tc = []pesticide.RegexpTestCase{
+		{Template: `{{ getHostByName "example.com" }}`, Regexp: ipAddressRegexp, Length: -1},
+		{Template: `{{ getHostByName "github.com" }}`, Regexp: ipAddressRegexp, Length: -1},
+		{Template: `{{ getHostByName "127.0.0.1" }}`, Regexp: ipAddressRegexp, Length: -1},
+	}
+
+	pesticide.RunRegexpTestCases(t, backward.NewRegistry(), tc)
+}
