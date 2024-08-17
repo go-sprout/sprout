fe63caec9369fd69f441ef4daef423290711d830
42Atomys <contact@atomys.fr>
2024-08-17 18:33:13 +0200

    test: improve the benchmark and compare tools to ensure compatibility

--- SPRIG_TO_SPROUT_CHANGES_NOTES.md
+++ SPRIG_TO_SPROUT_CHANGES_NOTES.md
@@ -10,100 +10,35 @@ It will be updated to reflect changes in future versions of the library.
 
 This document will also assist in creating the migration guide when version 1.0 is ready.
 
+Any changes between the fork date and version 1.0.0 of the Sprout library not 
+migrated yet to the [Migration from Sprig](https://docs.atom.codes/sprout/migration-from-sprig) will be documented here.
 
-## Error Handling Enhancements
-### General Error Handling
-In Sprig, errors within certain functions cause a panic. 
-In contrast, Sprout opts for returning nil or an empty value, improving safety
-and predictability.
 
-**Old Behavior (Sprig)**: Triggers a panic on error
-```go
-if err != nil {
-  panic("deepCopy error: " + err.Error())
-}
-```
+## Templating Differences
+### Bugs fixed
+In Sprig, somes functions have bugs that have been fixed in Sprout:
 
-**New Behavior (Sprout)**: Returns nil or an empty value on error
-```go
-if err != nil {
-  return nil, err
-}
-```
-
-Methods that previously caused a panic in Sprig :
-- DeepCopy
-- MustDeepCopy
-- ToRawJson
-- Append
-- Prepend
-- Concat
-- Chunk
-- Uniq
-- Compact
-- Slice
-- Without
-- Rest
-- Initial
-- Reverse
-- First
-- Last
-- Has
-- Dig
-- RandAlphaNumeric
-- RandAlpha
-- RandAscii
-- RandNumeric
-- RandBytes
-
-## Function-Specific Changes
-
-### MustDeepCopy
-
-- **Sprig**: Accepts `nil` input, causing an internal panic.
-- **Sprout**: Returns `nil` if input is `nil`, avoiding panic.
-
-## Rand Functions
-
-- **Sprig**: Causes an internal panic if the length parameter is zero.
-- **Sprout**: Returns an empty string if the length is zero, ensuring stability.
-
-## DateAgo
-
-- **Sprig**: Does not support int32 and *time.Time; returns "0s".
-- **Sprout**: Supports int32 and *time.Time and returns the correct duration.
-
-## DateRound
-- **Sprig**: Returns a corrected duration in positive form, even for negative inputs.
-- **Sprout**: Accurately returns the duration, preserving the sign of the input.
-
-## Base32Decode / Base64Decode
-- **Sprig**: Decoding functions return the error string when the input is not a valid base64 encoded string.
-- **Sprout**: Decoding functions return an empty string if the input is not a valid base64 encoded string, simplifying error handling.
-
-## Dig 
-> Consider the example dictionary defined as follows:
-> ```go
-> dict := map[string]any{
->   "a": map[string]any{
->     "b": 2,
->   },
-> }
-> ```
-
-- **Sprig**: Previously, the `dig` function would return the last map in the access chain.
-```go
-{{ $dict | dig "a" "b" }} // Output: map[b:2]
-```
-- **Sprout**: Now, the `dig` function returns the final object in the chain, regardless of its type (map, array, string, etc.).
-```go
-{{ $dict | dig "a" "b" }} // Output: 2
-```
-
-## ToCamelCase / ToPascalCase
-- **Sprig**: The `toCamelCase` return value are in PascalCase. No `toPascalCase` function is available.
-- **Sprout**: The `toCamelCase` function returns camelCase strings, while the `toPascalCase` function returns PascalCase strings.
-
-## Merge / MergeOverwrite
-- **Sprig**: The `merge` and `mergeOverwrite` functions does dereferencing when second value are the default golang value (example: `0` for int).
-- **Sprout**: The `merge` and `mergeOverwrite` functions does not dereference and keep the second value as is (example: `0` for int).
+1. `{{ "foooboooooo" | abbrevboth 4 9 }}`
+  - Sprig: `fooobo...`
+  - Sprout: `...boo...`
+2. `{{ "FoO  bar" | camelcase }}` | `{{ "foo  bar" | camelcase }}`
+  - Sprig: `FoO Bar` | `Foo Bar`
+  - Sprout: `FoOBar` | `FooBar`
+3. `{{ camelcase "___complex__case_" }}` | `{{ camelcase "_camel_case" }}`
+  - Sprig: `___Complex_Case_` | `_CamelCase`
+  - Sprout: `ComplexCase` | `CamelCase`
+4. `{{ "foo  bar" | kebabcase }}`
+  - Sprig: `foo--bar`
+  - Sprout: `foo-bar`
+5. `{{ "foo  bar" | snakecase }}`
+  - Sprig: `foo__bar`
+  - Sprout: `foo_bar`
+6. `{{ snakecase "Duration2m3s" }}` | `{{ snakecase "Bld4Floor3rd" }}`
+  - Sprig: `duration_2m3s` | `bld4_floor_3rd`
+  - Sprout: `duration_2m_3s` | `bld_4floor_3rd`
+7. `{{ "foobar" | substr 0 -3 }}` | `{{ "foobar" | substr -3 6 }}`
+  - Sprig: `foobar` | `foobar`
+  - Sprout: `foo` | `bar`
+8. `{{ .duration | durationRound }}`
+  - Sprig: `0s` (only when not casted to `time.Duration` before, `{{ .duration | duration | durationRound }}` works as expected with `5s`)
+  - Sprout: `5s`
--- benchmarks/comparaison_test.go
+++ benchmarks/comparaison_test.go
@@ -2,10 +2,13 @@ package benchmarks_test
 
 import (
 	"bytes"
+	"fmt"
 	"log/slog"
+	"runtime"
 	"sync"
 	"testing"
 	"text/template"
+	gotime "time"
 
 	"github.com/Masterminds/sprig/v3"
 	"github.com/go-sprout/sprout"
@@ -31,78 +34,116 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var data = map[string]any{
+	"string":      "example string value",
+	"url":         "https://example.com",
+	"arrayCommas": "a,b,c",
+	"int":         123,
+	"float":       123.456,
+	"bool":        true,
+	"array":       []any{"a", "b", "c"},
+	"map":         map[string]any{"foo": "bar"},
+	"object":      struct{ Name string }{"example object"},
+	"func":        func() string { return "example function" },
+	"error":       fmt.Errorf("example error"),
+	"time":        gotime.Now(),
+	"duration":    5 * gotime.Second,
+	"channel":     make(chan any),
+	"json":        `{"foo": "bar"}`,
+	"yaml":        "foo: bar",
+	"nil":         nil,
+}
+
+func BenchmarkComparison(b *testing.B) {
+	runtime.GOMAXPROCS(1)
+
+	b.Run("Sprig", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			sprigBench()
+		}
+	})
+
+	b.Run("Sprout", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			sproutBench("allFunctionsAsSprig.sprout.tmpl")
+		}
+	})
+}
+
+func BenchmarkAllSprout(b *testing.B) {
+	runtime.GOMAXPROCS(1)
+
+	b.Run("SproutWiothNewFeatures", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			sproutBench("allFunctions.sprout.tmpl")
+		}
+	})
+}
+
 /**
  * BenchmarkSprig are the benchmarks for Sprig.
  * It is the same as SproutBench but with Sprig.
  */
-func BenchmarkSprig(b *testing.B) {
-	b.ResetTimer()
+func sprigBench() {
+	tmpl, err := template.New("allFunctions").Funcs(sprig.FuncMap()).ParseGlob("allFunctions.sprig.tmpl")
+	if err != nil {
+		panic(err)
+	}
 
-	for i := 0; i < b.N; i++ {
-		tmpl, err := template.New("allFunctions").Funcs(sprig.FuncMap()).ParseGlob("*.sprig.tmpl")
+	var buf bytes.Buffer
+	for _, t := range tmpl.Templates() {
+		err := tmpl.ExecuteTemplate(&buf, t.Name(), data)
 		if err != nil {
 			panic(err)
 		}
-
-		var buf bytes.Buffer
-		for _, t := range tmpl.Templates() {
-			err := tmpl.ExecuteTemplate(&buf, t.Name(), nil)
-			if err != nil {
-				panic(err)
-			}
-		}
-
-		buf.Reset()
 	}
+
+	buf.Reset()
 }
 
 /**
  * BenchmarkSprout are the benchmarks for Sprout.
  */
-func BenchmarkSprout(b *testing.B) {
-	b.ResetTimer()
+func sproutBench(templatePath string) {
+	fnHandler := sprout.New(
+		sprout.WithLogger(slog.New(&slog.TextHandler{})),
+	)
 
-	for i := 0; i < b.N; i++ {
-		fnHandler := sprout.New(
-			sprout.WithLogger(slog.New(&slog.TextHandler{})),
-		)
+	_ = fnHandler.AddRegistries(
+		std.NewRegistry(),
+		uniqueid.NewRegistry(),
+		semver.NewRegistry(),
+		backward.NewRegistry(),
+		reflect.NewRegistry(),
+		time.NewRegistry(),
+		strings.NewRegistry(),
+		random.NewRegistry(),
+		checksum.NewRegistry(),
+		conversion.NewRegistry(),
+		numeric.NewRegistry(),
+		encoding.NewRegistry(),
+		regexp.NewRegistry(),
+		slices.NewRegistry(),
+		maps.NewRegistry(),
+		crypto.NewRegistry(),
+		filesystem.NewRegistry(),
+		env.NewRegistry(),
+	)
 
-		_ = fnHandler.AddRegistries(
-			std.NewRegistry(),
-			uniqueid.NewRegistry(),
-			semver.NewRegistry(),
-			backward.NewRegistry(),
-			reflect.NewRegistry(),
-			time.NewRegistry(),
-			strings.NewRegistry(),
-			random.NewRegistry(),
-			checksum.NewRegistry(),
-			conversion.NewRegistry(),
-			numeric.NewRegistry(),
-			encoding.NewRegistry(),
-			regexp.NewRegistry(),
-			slices.NewRegistry(),
-			maps.NewRegistry(),
-			crypto.NewRegistry(),
-			filesystem.NewRegistry(),
-			env.NewRegistry(),
-		)
+	tmpl, err := template.New("allFunctions").Funcs(fnHandler.Build()).ParseGlob(templatePath)
 
-		tmpl, err := template.New("allFunctions").Funcs(fnHandler.Build()).ParseGlob("*.sprout.tmpl")
+	if err != nil {
+		panic(err)
+	}
 
+	var buf bytes.Buffer
+	for _, t := range tmpl.Templates() {
+		err := tmpl.ExecuteTemplate(&buf, t.Name(), data)
 		if err != nil {
 			panic(err)
 		}
-
-		var buf bytes.Buffer
-		for _, t := range tmpl.Templates() {
-			err := tmpl.ExecuteTemplate(&buf, t.Name(), fnHandler)
-			if err != nil {
-				panic(err)
-			}
-		}
-		buf.Reset()
 	}
+	buf.Reset()
 }
 
 func TestCompareSprigAndSprout(t *testing.T) {
@@ -118,7 +159,7 @@ func TestCompareSprigAndSprout(t *testing.T) {
 		tmplSprig, err := template.New("compare").Funcs(sprig.FuncMap()).ParseFiles("compare.tmpl")
 		assert.NoError(t, err)
 
-		err = tmplSprig.ExecuteTemplate(&bufSprig, "compare.tmpl", nil)
+		err = tmplSprig.ExecuteTemplate(&bufSprig, "compare.tmpl", data)
 		assert.NoError(t, err)
 	}()
 
@@ -128,11 +169,11 @@ func TestCompareSprigAndSprout(t *testing.T) {
 		tmplSprout, err := template.New("compare").Funcs(sprigin.FuncMap()).ParseGlob("compare.tmpl")
 		assert.NoError(t, err)
 
-		err = tmplSprout.ExecuteTemplate(&bufSprout, "compare.tmpl", nil)
+		err = tmplSprout.ExecuteTemplate(&bufSprout, "compare.tmpl", data)
 		assert.NoError(t, err)
 	}()
 
 	wg.Wait()
-	// sprig is expected and sprout is actual
+	// sprig is expected (---) and sprout is actual (+++)
 	assert.Equal(t, bufSprig.String(), bufSprout.String())
 }
