fbeee42d7579ebd35c224c934f522fa49d97f4bc
42Atomys <contact@atomys.fr>
2024-09-01 23:58:21 +0200

    chore: migrate std registry

--- registry/std/functions.go
+++ registry/std/functions.go
@@ -9,8 +9,8 @@ import (
 
 // Hello returns a greeting string.
 // It simply returns the string "Hello!" to be used as a test function.
-func (sr *StdRegistry) Hello() string {
-	return "Hello!"
+func (sr *StdRegistry) Hello() (string, error) {
+	return "Hello!", nil
 }
 
 // Default returns the first non-empty value from the given arguments or a
@@ -33,6 +33,7 @@ func (sr *StdRegistry) Hello() string {
 //
 //	any - the first element of `given`, or `defaultValue` if `given` is empty
 //	      or all values are empty.
+//	error - an error if the number of arguments is less than 1.
 //
 // Example:
 //
@@ -40,11 +41,11 @@ func (sr *StdRegistry) Hello() string {
 //	{{ "" | default "default" }}  // Output: "default"
 //	{{ "first" | default "default" }} // Output: "first"
 //	{{ "first" | default "default" "second" }} // Output: "second"
-func (sr *StdRegistry) Default(defaultValue any, given ...any) any {
+func (sr *StdRegistry) Default(defaultValue any, given ...any) (any, error) {
 	if len(given) == 0 || helpers.Empty(given[0]) {
-		return defaultValue
+		return defaultValue, nil
 	}
-	return given[0]
+	return given[0], nil
 }
 
 // Empty evaluates the emptiness of the provided value 'given'. It returns
@@ -59,6 +60,7 @@ func (sr *StdRegistry) Default(defaultValue any, given ...any) any {
 // Returns:
 //
 //	bool - true if 'given' is empty, false otherwise.
+//	error - placeholder for potential errors in the future.
 //
 // This method utilizes the reflect package to inspect the type and value of
 // 'given'. Depending on the type, it checks for nil pointers, zero-length
@@ -73,8 +75,8 @@ func (sr *StdRegistry) Default(defaultValue any, given ...any) any {
 //	{{ 0 | empty }} // Output: true
 //	{{ false | empty }} // Output: true
 //	{{ struct{}{} | empty }} // Output: false
-func (sr *StdRegistry) Empty(given any) bool {
-	return helpers.Empty(given)
+func (sr *StdRegistry) Empty(given any) (bool, error) {
+	return helpers.Empty(given), nil
 }
 
 // All checks if all values in the provided variadic slice are non-empty.
@@ -87,62 +89,65 @@ func (sr *StdRegistry) Empty(given any) bool {
 // Returns:
 //
 //	bool - true if all values are non-empty, false otherwise.
+//	error - placeholder for potential errors in the future.
 //
 // Example:
 //
 //	{{ 1, "hello", true | all }} // Output: true
 //	{{ 1, "", true | all }} // Output: false
-func (sr *StdRegistry) All(values ...any) bool {
+func (sr *StdRegistry) All(values ...any) (bool, error) {
 	for _, val := range values {
 		if helpers.Empty(val) {
-			return false
+			return false, nil
 		}
 	}
-	return true
+	return true, nil
 }
 
 // Any checks if any of the provided values are non-empty.
 // It returns true if at least one value is non-empty.
 //
 // Parameters:
-//   values ...any - a variadic parameter list of values to be checked.
+//	values ...any - a variadic parameter list of values to be checked.
 //
 // Returns:
-//   bool - true if any value is non-empty, false if all are empty.
+//	bool - true if any value is non-empty, false if all are empty.
+//	error - placeholder for potential errors in the future.
 //
 // Example:
-//   {{ "", 0, false | any }} // Output: false
-//   {{ "", 0, "text" | any }} // Output: true
+//	{{ "", 0, false | any }} // Output: false
+//	{{ "", 0, "text" | any }} // Output: true
 
-func (sr *StdRegistry) Any(values ...any) bool {
+func (sr *StdRegistry) Any(values ...any) (bool, error) {
 	for _, val := range values {
 		if !helpers.Empty(val) {
-			return true
+			return true, nil
 		}
 	}
-	return false
+	return false, nil
 }
 
 // Coalesce returns the first non-empty value from the given list.
 // If all values are empty, it returns nil.
 //
 // Parameters:
-//   values ...any - a variadic parameter list of values from which the first
+//	values ...any - a variadic parameter list of values from which the first
 //                   non-empty value should be selected.
 //
 // Returns:
-//   any - the first non-empty value, or nil if all values are empty.
+//	any - the first non-empty value, or nil if all values are empty.
+//	error - placeholder for potential errors in the future.
 //
 // Example:
-//   {{ nil, "", "first", "second" | coalesce }} // Output: "first"
+//	{{ nil, "", "first", "second" | coalesce }} // Output: "first"
 
-func (sr *StdRegistry) Coalesce(values ...any) any {
+func (sr *StdRegistry) Coalesce(values ...any) (any, error) {
 	for _, val := range values {
 		if !helpers.Empty(val) {
-			return val
+			return val, nil
 		}
 	}
-	return nil
+	return nil, nil
 }
 
 // Ternary mimics the ternary conditional operator found in many programming languages.
@@ -157,17 +162,18 @@ func (sr *StdRegistry) Coalesce(values ...any) any {
 // Returns:
 //
 //	any - the result based on the evaluated condition.
+//	error - placeholder for potential errors in the future.
 //
 // Example:
 //
 //	{{ "yes", "no", true | ternary }} // Output: "yes"
 //	{{ "yes", "no", false | ternary }} // Output: "no"
-func (sr *StdRegistry) Ternary(trueValue any, falseValue any, condition bool) any {
+func (sr *StdRegistry) Ternary(trueValue any, falseValue any, condition bool) (any, error) {
 	if condition {
-		return trueValue
+		return trueValue, nil
 	}
 
-	return falseValue
+	return falseValue, nil
 }
 
 // Cat concatenates a series of values into a single string. Each value is
@@ -182,11 +188,12 @@ func (sr *StdRegistry) Ternary(trueValue any, falseValue any, condition bool) an
 //
 //	string - a single string composed of all non-nil input values separated
 //	         by spaces.
+//	error - placeholder for potential errors in the future.
 //
 // Example:
 //
 //	{{ "Hello", nil, 123, true | cat }} // Output: "Hello 123 true"
-func (sr *StdRegistry) Cat(values ...any) string {
+func (sr *StdRegistry) Cat(values ...any) (string, error) {
 	var builder strings.Builder
 	for i, item := range values {
 		if item == nil {
@@ -199,5 +206,5 @@ func (sr *StdRegistry) Cat(values ...any) string {
 		builder.WriteString(fmt.Sprint(item))
 	}
 	// Return the concatenated string without trailing spaces
-	return builder.String()
+	return builder.String(), nil
 }
--- registry/std/functions_test.go
+++ registry/std/functions_test.go
@@ -9,118 +9,118 @@ import (
 
 // TestHello asserts the Hello method returns the expected greeting.
 func TestHello(t *testing.T) {
-	var tc = []pesticide.SafeTestCase{
-		{Name: "TestHello", Input: `{{hello}}`, Expected: "Hello!"},
+	var tc = []pesticide.TestCase{
+		{Name: "TestHello", Input: `{{hello}}`, ExpectedOutput: "Hello!"},
 	}
-	pesticide.RunSafeTestCases(t, std.NewRegistry(), tc)
+	pesticide.RunTestCases(t, std.NewRegistry(), tc)
 }
 
 func TestDefault(t *testing.T) {
-	var tc = []pesticide.SafeTestCase{
-		{Name: "TestDefaultEmptyInput", Input: `{{default "default" ""}}`, Expected: "default"},
-		{Name: "TestDefaultGivenInput", Input: `{{default "default" "given"}}`, Expected: "given"},
-		{Name: "TestDefaultIntInput", Input: `{{default "default" 42}}`, Expected: "42"},
-		{Name: "TestDefaultFloatInput", Input: `{{default "default" 2.42}}`, Expected: "2.42"},
-		{Name: "TestDefaultTrueInput", Input: `{{default "default" true}}`, Expected: "true"},
-		{Name: "TestDefaultFalseInput", Input: `{{default "default" false}}`, Expected: "default"},
-		{Name: "TestDefaultNilInput", Input: `{{default "default" nil}}`, Expected: "default"},
-		{Name: "TestDefaultNothingInput", Input: `{{default "default" .Nothing}}`, Expected: "default"},
-		{Name: "TestDefaultMultipleNothingInput", Input: `{{default "default" .Nothing}}`, Expected: "default"},
-		{Name: "TestDefaultMultipleArgument", Input: `{{"first" | default "default" "second"}}`, Expected: "second"},
+	var tc = []pesticide.TestCase{
+		{Name: "TestDefaultEmptyInput", Input: `{{default "default" ""}}`, ExpectedOutput: "default"},
+		{Name: "TestDefaultGivenInput", Input: `{{default "default" "given"}}`, ExpectedOutput: "given"},
+		{Name: "TestDefaultIntInput", Input: `{{default "default" 42}}`, ExpectedOutput: "42"},
+		{Name: "TestDefaultFloatInput", Input: `{{default "default" 2.42}}`, ExpectedOutput: "2.42"},
+		{Name: "TestDefaultTrueInput", Input: `{{default "default" true}}`, ExpectedOutput: "true"},
+		{Name: "TestDefaultFalseInput", Input: `{{default "default" false}}`, ExpectedOutput: "default"},
+		{Name: "TestDefaultNilInput", Input: `{{default "default" nil}}`, ExpectedOutput: "default"},
+		{Name: "TestDefaultNothingInput", Input: `{{default "default" .Nothing}}`, ExpectedOutput: "default"},
+		{Name: "TestDefaultMultipleNothingInput", Input: `{{default "default" .Nothing}}`, ExpectedOutput: "default"},
+		{Name: "TestDefaultMultipleArgument", Input: `{{"first" | default "default" "second"}}`, ExpectedOutput: "second"},
 	}
 
-	pesticide.RunSafeTestCases(t, std.NewRegistry(), tc)
+	pesticide.RunTestCases(t, std.NewRegistry(), tc)
 }
 
 func TestEmpty(t *testing.T) {
-	var tc = []pesticide.SafeTestCase{
-		{Name: "TestEmptyEmptyInput", Input: `{{if empty ""}}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestEmptyGivenInput", Input: `{{if empty "given"}}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestEmptyIntInput", Input: `{{if empty 42}}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestEmptyUintInput", Input: `{{if empty .i}}1{{else}}0{{end}}`, Expected: "0", Data: map[string]any{"i": uint(42)}},
-		{Name: "TestEmptyComplexInput", Input: `{{if empty .c}}1{{else}}0{{end}}`, Expected: "0", Data: map[string]any{"c": complex(42, 42)}},
-		{Name: "TestEmptyFloatInput", Input: `{{if empty 2.42}}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestEmptyTrueInput", Input: `{{if empty true}}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestEmptyFalseInput", Input: `{{if empty false}}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestEmptyStructInput", Input: `{{if empty .s}}1{{else}}0{{end}}`, Expected: "0", Data: map[string]any{"s": struct{}{}}},
-		{Name: "TestEmptyNilInput", Input: `{{if empty nil}}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestEmptyNothingInput", Input: `{{if empty .Nothing}}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestEmptyNestedInput", Input: `{{if empty .top.NoSuchThing}}1{{else}}0{{end}}`, Expected: "1", Data: map[string]any{"top": map[string]any{}}},
-		{Name: "TestEmptyNestedNoDataInput", Input: `{{if empty .bottom.NoSuchThing}}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestEmptyNimPointerInput", Input: `{{if empty .nilPtr}}1{{else}}0{{end}}`, Expected: "1", Data: map[string]any{"nilPtr": (*int)(nil)}},
+	var tc = []pesticide.TestCase{
+		{Name: "TestEmptyEmptyInput", Input: `{{if empty ""}}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestEmptyGivenInput", Input: `{{if empty "given"}}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestEmptyIntInput", Input: `{{if empty 42}}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestEmptyUintInput", Input: `{{if empty .i}}1{{else}}0{{end}}`, ExpectedOutput: "0", Data: map[string]any{"i": uint(42)}},
+		{Name: "TestEmptyComplexInput", Input: `{{if empty .c}}1{{else}}0{{end}}`, ExpectedOutput: "0", Data: map[string]any{"c": complex(42, 42)}},
+		{Name: "TestEmptyFloatInput", Input: `{{if empty 2.42}}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestEmptyTrueInput", Input: `{{if empty true}}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestEmptyFalseInput", Input: `{{if empty false}}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestEmptyStructInput", Input: `{{if empty .s}}1{{else}}0{{end}}`, ExpectedOutput: "0", Data: map[string]any{"s": struct{}{}}},
+		{Name: "TestEmptyNilInput", Input: `{{if empty nil}}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestEmptyNothingInput", Input: `{{if empty .Nothing}}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestEmptyNestedInput", Input: `{{if empty .top.NoSuchThing}}1{{else}}0{{end}}`, ExpectedOutput: "1", Data: map[string]any{"top": map[string]any{}}},
+		{Name: "TestEmptyNestedNoDataInput", Input: `{{if empty .bottom.NoSuchThing}}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestEmptyNimPointerInput", Input: `{{if empty .nilPtr}}1{{else}}0{{end}}`, ExpectedOutput: "1", Data: map[string]any{"nilPtr": (*int)(nil)}},
 	}
 
-	pesticide.RunSafeTestCases(t, std.NewRegistry(), tc)
+	pesticide.RunTestCases(t, std.NewRegistry(), tc)
 }
 
 func TestAll(t *testing.T) {
-	var tc = []pesticide.SafeTestCase{
-		{Name: "TestAllEmptyInput", Input: `{{if all ""}}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestAllGivenInput", Input: `{{if all "given"}}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestAllIntInput", Input: `{{if all 42 0 1}}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestAllVariableInput1", Input: `{{ $two := 2 }}{{if all "" 0 nil $two }}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestAllVariableInput2", Input: `{{ $two := 2 }}{{if all "" $two 0 0 0 }}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestAllVariableInput3", Input: `{{ $two := 2 }}{{if all "" $two 3 4 5 }}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestAllNoInput", Input: `{{if all }}1{{else}}0{{end}}`, Expected: "1"},
+	var tc = []pesticide.TestCase{
+		{Name: "TestAllEmptyInput", Input: `{{if all ""}}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestAllGivenInput", Input: `{{if all "given"}}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestAllIntInput", Input: `{{if all 42 0 1}}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestAllVariableInput1", Input: `{{ $two := 2 }}{{if all "" 0 nil $two }}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestAllVariableInput2", Input: `{{ $two := 2 }}{{if all "" $two 0 0 0 }}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestAllVariableInput3", Input: `{{ $two := 2 }}{{if all "" $two 3 4 5 }}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestAllNoInput", Input: `{{if all }}1{{else}}0{{end}}`, ExpectedOutput: "1"},
 	}
 
-	pesticide.RunSafeTestCases(t, std.NewRegistry(), tc)
+	pesticide.RunTestCases(t, std.NewRegistry(), tc)
 }
 
 func TestAny(t *testing.T) {
-	var tc = []pesticide.SafeTestCase{
-		{Name: "TestAnyEmptyInput", Input: `{{if any ""}}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestAnyGivenInput", Input: `{{if any "given"}}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestAnyIntInput", Input: `{{if any 42 0 1}}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestAnyVariableInput1", Input: `{{ $two := 2 }}{{if any "" 0 nil $two }}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestAnyVariableInput2", Input: `{{ $two := 2 }}{{if any "" $two 3 4 5 }}1{{else}}0{{end}}`, Expected: "1"},
-		{Name: "TestAnyVariableInput3", Input: `{{ $zero := 0 }}{{if any "" $zero 0 0 0 }}1{{else}}0{{end}}`, Expected: "0"},
-		{Name: "TestAnyNoInput", Input: `{{if any }}1{{else}}0{{end}}`, Expected: "0"},
+	var tc = []pesticide.TestCase{
+		{Name: "TestAnyEmptyInput", Input: `{{if any ""}}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestAnyGivenInput", Input: `{{if any "given"}}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestAnyIntInput", Input: `{{if any 42 0 1}}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestAnyVariableInput1", Input: `{{ $two := 2 }}{{if any "" 0 nil $two }}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestAnyVariableInput2", Input: `{{ $two := 2 }}{{if any "" $two 3 4 5 }}1{{else}}0{{end}}`, ExpectedOutput: "1"},
+		{Name: "TestAnyVariableInput3", Input: `{{ $zero := 0 }}{{if any "" $zero 0 0 0 }}1{{else}}0{{end}}`, ExpectedOutput: "0"},
+		{Name: "TestAnyNoInput", Input: `{{if any }}1{{else}}0{{end}}`, ExpectedOutput: "0"},
 	}
 
-	pesticide.RunSafeTestCases(t, std.NewRegistry(), tc)
+	pesticide.RunTestCases(t, std.NewRegistry(), tc)
 }
 
 func TestCoalesce(t *testing.T) {
-	var tc = []pesticide.SafeTestCase{
-		{Name: "TestCoalesceEmptyInput", Input: `{{coalesce ""}}`, Expected: "<no value>"},
-		{Name: "TestCoalesceGivenInput", Input: `{{coalesce "given"}}`, Expected: "given"},
-		{Name: "TestCoalesceIntInput", Input: `{{ coalesce "" 0 nil 42 }}`, Expected: "42"},
-		{Name: "TestCoalesceVariableInput1", Input: `{{ $two := 2 }}{{ coalesce "" 0 nil $two }}`, Expected: "2"},
-		{Name: "TestCoalesceVariableInput2", Input: `{{ $two := 2 }}{{ coalesce "" $two 0 0 0 }}`, Expected: "2"},
-		{Name: "TestCoalesceVariableInput3", Input: `{{ $two := 2 }}{{ coalesce "" $two 3 4 5 }}`, Expected: "2"},
-		{Name: "TestCoalesceNoInput", Input: `{{ coalesce }}`, Expected: "<no value>"},
+	var tc = []pesticide.TestCase{
+		{Name: "TestCoalesceEmptyInput", Input: `{{coalesce ""}}`, ExpectedOutput: "<no value>"},
+		{Name: "TestCoalesceGivenInput", Input: `{{coalesce "given"}}`, ExpectedOutput: "given"},
+		{Name: "TestCoalesceIntInput", Input: `{{ coalesce "" 0 nil 42 }}`, ExpectedOutput: "42"},
+		{Name: "TestCoalesceVariableInput1", Input: `{{ $two := 2 }}{{ coalesce "" 0 nil $two }}`, ExpectedOutput: "2"},
+		{Name: "TestCoalesceVariableInput2", Input: `{{ $two := 2 }}{{ coalesce "" $two 0 0 0 }}`, ExpectedOutput: "2"},
+		{Name: "TestCoalesceVariableInput3", Input: `{{ $two := 2 }}{{ coalesce "" $two 3 4 5 }}`, ExpectedOutput: "2"},
+		{Name: "TestCoalesceNoInput", Input: `{{ coalesce }}`, ExpectedOutput: "<no value>"},
 	}
 
-	pesticide.RunSafeTestCases(t, std.NewRegistry(), tc)
+	pesticide.RunTestCases(t, std.NewRegistry(), tc)
 }
 
 func TestTernary(t *testing.T) {
-	var tc = []pesticide.SafeTestCase{
-		{Input: `{{true | ternary "foo" "bar"}}`, Expected: "foo"},
-		{Input: `{{ternary "foo" "bar" true}}`, Expected: "foo"},
-		{Input: `{{false | ternary "foo" "bar"}}`, Expected: "bar"},
-		{Input: `{{ternary "foo" "bar" false}}`, Expected: "bar"},
+	var tc = []pesticide.TestCase{
+		{Input: `{{true | ternary "foo" "bar"}}`, ExpectedOutput: "foo"},
+		{Input: `{{ternary "foo" "bar" true}}`, ExpectedOutput: "foo"},
+		{Input: `{{false | ternary "foo" "bar"}}`, ExpectedOutput: "bar"},
+		{Input: `{{ternary "foo" "bar" false}}`, ExpectedOutput: "bar"},
 	}
 
-	pesticide.RunSafeTestCases(t, std.NewRegistry(), tc)
+	pesticide.RunTestCases(t, std.NewRegistry(), tc)
 }
 
 func TestCat(t *testing.T) {
-	var tc = []pesticide.SafeTestCase{
-		{Name: "TestCatEmptyInput", Input: `{{cat ""}}`, Expected: ""},
-		{Name: "TestCatGivenInput", Input: `{{cat "given"}}`, Expected: "given"},
-		{Name: "TestCatIntInput", Input: `{{cat 42}}`, Expected: "42"},
-		{Name: "TestCatFloatInput", Input: `{{cat 2.42}}`, Expected: "2.42"},
-		{Name: "TestCatTrueInput", Input: `{{cat true}}`, Expected: "true"},
-		{Name: "TestCatFalseInput", Input: `{{cat false}}`, Expected: "false"},
-		{Name: "TestCatNilInput", Input: `{{cat nil}}`, Expected: ""},
-		{Name: "TestCatNothingInput", Input: `{{cat .Nothing}}`, Expected: ""},
-		{Name: "TestCatMultipleInput", Input: `{{cat "first" "second"}}`, Expected: "first second"},
-		{Name: "TestCatMultipleArgument", Input: `{{"first" | cat "second"}}`, Expected: "second first"},
-		{Name: "TestCatVariableInput", Input: `{{$b := "b"}}{{"c" | cat "a" $b}}`, Expected: "a b c"},
-		{Name: "TestCatDataInput", Input: `{{.text | cat "a" "b"}}`, Expected: "a b cd", Data: map[string]any{"text": "cd"}},
+	var tc = []pesticide.TestCase{
+		{Name: "TestCatEmptyInput", Input: `{{cat ""}}`, ExpectedOutput: ""},
+		{Name: "TestCatGivenInput", Input: `{{cat "given"}}`, ExpectedOutput: "given"},
+		{Name: "TestCatIntInput", Input: `{{cat 42}}`, ExpectedOutput: "42"},
+		{Name: "TestCatFloatInput", Input: `{{cat 2.42}}`, ExpectedOutput: "2.42"},
+		{Name: "TestCatTrueInput", Input: `{{cat true}}`, ExpectedOutput: "true"},
+		{Name: "TestCatFalseInput", Input: `{{cat false}}`, ExpectedOutput: "false"},
+		{Name: "TestCatNilInput", Input: `{{cat nil}}`, ExpectedOutput: ""},
+		{Name: "TestCatNothingInput", Input: `{{cat .Nothing}}`, ExpectedOutput: ""},
+		{Name: "TestCatMultipleInput", Input: `{{cat "first" "second"}}`, ExpectedOutput: "first second"},
+		{Name: "TestCatMultipleArgument", Input: `{{"first" | cat "second"}}`, ExpectedOutput: "second first"},
+		{Name: "TestCatVariableInput", Input: `{{$b := "b"}}{{"c" | cat "a" $b}}`, ExpectedOutput: "a b c"},
+		{Name: "TestCatDataInput", Input: `{{.text | cat "a" "b"}}`, ExpectedOutput: "a b cd", Data: map[string]any{"text": "cd"}},
 	}
 
-	pesticide.RunSafeTestCases(t, std.NewRegistry(), tc)
+	pesticide.RunTestCases(t, std.NewRegistry(), tc)
 }
