f62ee092955d7476754e0f58c99c8e82ce2dcd70
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2024-12-06 11:19:53 +0100

    feat: use uppercase acronyms in template functions (#98)
    ## Description
    
    See discussions in 
    - #84 
    
    ## Changes
    
    The functions are now using uppercase acronym, old ones are aliased, but
    marked as deprecated.
    
    ## Fixes #84
    
    ## Checklist
    - [X] I have read the **CONTRIBUTING.md** document.
    - [X] My code follows the code style of this project.
    - [ ] I have added tests to cover my changes.
    - [X] All new and existing tests passed.
    - [X] I have updated the documentation accordingly.
    - [X] This change requires a change to the documentation on the website.
    
    ## Additional Information
    <!-- Any additional information regarding this pull request. -->
    
    ---------
    
    Co-authored-by: Atomys <contact@atomys.fr>

--- registry/encoding/encoding.go
+++ registry/encoding/encoding.go
@@ -27,32 +27,40 @@ func (er *EncodingRegistry) RegisterFunctions(funcsMap sprout.FunctionMap) error
 	sprout.AddFunction(funcsMap, "base64Decode", er.Base64Decode)
 	sprout.AddFunction(funcsMap, "base32Encode", er.Base32Encode)
 	sprout.AddFunction(funcsMap, "base32Decode", er.Base32Decode)
-	sprout.AddFunction(funcsMap, "fromJson", er.FromJson)
-	sprout.AddFunction(funcsMap, "toJson", er.ToJson)
-	sprout.AddFunction(funcsMap, "toPrettyJson", er.ToPrettyJson)
-	sprout.AddFunction(funcsMap, "toRawJson", er.ToRawJson)
-	sprout.AddFunction(funcsMap, "fromYaml", er.FromYAML)
-	sprout.AddFunction(funcsMap, "toYaml", er.ToYAML)
-	sprout.AddFunction(funcsMap, "toIndentYaml", er.ToIndentYAML)
+	sprout.AddFunction(funcsMap, "fromJSON", er.FromJSON)
+	sprout.AddFunction(funcsMap, "toJSON", er.ToJSON)
+	sprout.AddFunction(funcsMap, "toPrettyJSON", er.ToPrettyJSON)
+	sprout.AddFunction(funcsMap, "toRawJSON", er.ToRawJSON)
+	sprout.AddFunction(funcsMap, "fromYAML", er.FromYAML)
+	sprout.AddFunction(funcsMap, "toYAML", er.ToYAML)
+	sprout.AddFunction(funcsMap, "toIndentYAML", er.ToIndentYAML)
 	return nil
 }
 
 func (er *EncodingRegistry) RegisterAliases(aliasesMap sprout.FunctionAliasMap) error {
-	sprout.AddAlias(aliasesMap, "fromJson", "mustFromJson")
-	sprout.AddAlias(aliasesMap, "toJson", "mustToJson")
-	sprout.AddAlias(aliasesMap, "toPrettyJson", "mustToPrettyJson")
-	sprout.AddAlias(aliasesMap, "toRawJson", "mustToRawJson")
-	sprout.AddAlias(aliasesMap, "fromYaml", "mustFromYaml")
-	sprout.AddAlias(aliasesMap, "toYaml", "mustToYaml")
+	sprout.AddAlias(aliasesMap, "fromJSON", "fromJson", "mustFromJson")
+	sprout.AddAlias(aliasesMap, "toJSON", "toJson", "mustToJson")
+	sprout.AddAlias(aliasesMap, "toPrettyJSON", "toPrettyJson", "mustToPrettyJson")
+	sprout.AddAlias(aliasesMap, "toRawJSON", "toRawJson", "mustToRawJson")
+	sprout.AddAlias(aliasesMap, "fromYAML", "fromYaml", "mustFromYaml")
+	sprout.AddAlias(aliasesMap, "toYAML", "toYaml", "mustToYaml")
 	return nil
 }
 
 func (er *EncodingRegistry) RegisterNotices(notices *[]sprout.FunctionNotice) error {
-	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustFromJson", "please use `fromJson` instead"))
-	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustToJson", "please use `toJson` instead"))
-	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustToPrettyJson", "please use `toPrettyJson` instead"))
-	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustToRawJson", "please use `toRawJson` instead"))
-	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustFromYaml", "please use `fromYaml` instead"))
-	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustToYaml", "please use `toYaml` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustFromJson", "please use `fromJSON` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustToJson", "please use `toJSON` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustToPrettyJson", "please use `toPrettyJSON` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustToRawJson", "please use `toRawJSON` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustFromYaml", "please use `fromYAML` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("mustToYaml", "please use `toYAML` instead"))
+
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("fromJson", "please use `fromJSON` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("toJson", "please use `toJSON` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("toPrettyJson", "please use `toPrettyJSON` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("toRawJson", "please use `toRawJSON` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("fromYaml", "please use `fromYAML` instead"))
+	sprout.AddNotice(notices, sprout.NewDeprecatedNotice("toYaml", "please use `toYAML` instead"))
+
 	return nil
 }
