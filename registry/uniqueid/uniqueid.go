4ffa15b0461b5ff57c4616c1b7e3a5f0517b38c2
Atomys <contact@atomys.fr>
2026-08-16 01:31:53 +0200

    feat: new uniqueid functions (uuid)

--- registry/uniqueid/uniqueid.go
+++ registry/uniqueid/uniqueid.go
@@ -25,5 +25,12 @@ func (ur *UniqueIDRegistry) LinkHandler(fh sprout.Handler) error {
 // RegisterFunctions registers all functions of the registry.
 func (ur *UniqueIDRegistry) RegisterFunctions(funcsMap sprout.FunctionMap) error {
 	sprout.AddFunction(funcsMap, "uuidv4", ur.Uuidv4)
+	sprout.AddFunction(funcsMap, "uuidv7", ur.Uuidv7)
+	sprout.AddFunction(funcsMap, "uuidv5", ur.Uuidv5)
+	sprout.AddFunction(funcsMap, "uuidv3", ur.Uuidv3)
+	sprout.AddFunction(funcsMap, "uuidNil", ur.UuidNil)
+	sprout.AddFunction(funcsMap, "isUUID", ur.IsUUID)
+	sprout.AddFunction(funcsMap, "uuidVersion", ur.UuidVersion)
+	sprout.AddFunction(funcsMap, "uuidTime", ur.UuidTime)
 	return nil
 }
